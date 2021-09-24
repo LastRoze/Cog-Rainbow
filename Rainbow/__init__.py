f5e9d1deb5fe6477a53fb1541fe71b05d0b325a4
Yoga Budiman <LastRoze@users.noreply.github.com>
2021-09-24 18:34:21 +0800

    Delete __init__.py

--- Rainbow/__init__.py
+++
@@ -1,9 +0,0 @@
-from .rainbow import Rainbow
-
-__red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
-
-
-async def setup(bot):
-    cog = Rainbow(bot)
-    bot.add_cog(cog)
-    await cog.initialise()
