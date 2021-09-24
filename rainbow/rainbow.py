d9a63c68c548e39db0a6f61e22cca2516c97d8ed
Yoga Budiman <LastRoze@users.noreply.github.com>
2021-09-24 19:24:18 +0800

    Update rainbow.py

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -4,16 +4,14 @@ import asyncio
 from redbot.core import checks, commands
 from random import choice, randint
 
-__author__ = "Last Roze"
 class rainbow:
-    """Make your role colorful!"""
-
     def __init__(self, bot):
         self.bot = bot
 
+	@checks.admin_or_permissions(manage_roles=True)
     @commands.command(administrator=True)
     async def arainbow(self, ctx, interval:float, *, role):
-        roleObj = discord.utils.find(lambda r: r.name == role, ctx.message.guild.roles)
+        roleObj = discord.utils.find(lambda r: r.name == role, ctx.message.server.roles)
         if not roleObj:
             no = discord.Embed(title="{} is not a valid role".format(role))
             await self.bot.say(embed=no)
@@ -23,14 +21,15 @@ class rainbow:
         while True:
             colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
             colour = int(colour, 16)
-            await self.bot.edit_role(ctx.message.guild, roleObj, colour=discord.Colour(value=colour))
+            await self.bot.edit_role(ctx.message.server, roleObj, colour=discord.Colour(value=colour))
             await asyncio.sleep(interval)
 
+	@checks.admin_or_permissions(manage_roles=True)
     @commands.command(administrator=True)
     async def rainbow(self, ctx, *, role: discord.Role):
 
         while True:
             colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
             colour = int(colour, 16)
-            await self.bot.edit_role(ctx.message.guild, role, colour=discord.Colour(value=colour))
+            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
             await asyncio.sleep(0.0)
